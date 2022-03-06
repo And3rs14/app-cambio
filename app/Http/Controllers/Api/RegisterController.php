1c3a95c72b3ecc7c7b323659958a1fe0bc301cb2
Bill Anderson <billbardales@gmail.com>
2022-03-06 18:19:09 -0500

    Agregando el proyecto

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+Use App\Models\User;
+
+class RegisterController extends Controller
+{
+    
+    public function store(Request $request){
+        $request->validate([
+            'name'=> 'required|string|max:255',
+            'name'=> 'required|string|email:255|unique:users',
+            'password'=> 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create($request->all());
+
+        return response($user, 200);
+    }
+
+}
