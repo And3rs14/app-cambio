fbd8f9642e0e2cc878fad5603666deb2de728484
Bill Anderson <billbardales@gmail.com>
2022-03-13 02:23:36 -0500

    Relacion de 1 a muchos entre Category y Value

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    //Habilitar asignacion masiva
+    protected $fillable =['name'];
+
+    //Relación de 1 a muchos 
+    public function values(){
+        return $this->hasMany(Value::class);
+    }
 }
