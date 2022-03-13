9efec1179520a04685ef8087f92ec73a748f6fe9
Bill Anderson <billbardales@gmail.com>
2022-03-12 21:29:59 -0500

    Tercer arreglo en RegisterController

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -10,11 +10,12 @@ class RegisterController extends Controller
 {
     
     public function store(Request $request){
-        $request->validate([
+        $request->validate(
+            [
             'name'=> 'required|string|max:255',
             'email'=> 'required|string|email|max:255|unique:users',
             'password'=> 'required|string|min:8|confirmed',
-        ]
+            ]
     );
 
         $user = User::create($request->all());
