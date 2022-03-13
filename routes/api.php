ad5f58f245d304b02dcd9b18075913fbcac28734
Bill Anderson <billbardales@gmail.com>
2022-03-12 22:16:07 -0500

    Cuarto arreglo en RegisterController

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Use App\Http\Controllers\Api\RegisterController;
+
+Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +18,7 @@ Use App\Http\Controllers\Api\RegisterController;
 |
 */
 
-Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
