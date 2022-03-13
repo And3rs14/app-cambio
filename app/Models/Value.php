c06335ed7dd36300a91af44fa0bec0ee8603fb4a
Bill Anderson <billbardales@gmail.com>
2022-03-13 02:29:37 -0500

    Relacion de 1 a muchos inversa entre Value y las demas tablas

--- app/Models/Value.php
+++ app/Models/Value.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Value extends Model
 {
     use HasFactory;
+
+     //relacion de 1 a muchos inversa 
+     public function user()
+     {
+         return $this->belongsTo(User::class);
+     }
+     
+     public function category()
+     {
+         return $this->belongsTo(Category::class);
+     }
+     public function date()
+     {
+         return $this->belongsTo(Date::class);
+     }
 }
