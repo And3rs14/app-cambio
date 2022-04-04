ba9eb3462eaa0515a29285a9b675d776ede327f8
Bill Anderson <billbardales@gmail.com>
2022-04-04 14:34:06 -0500

    Creacion de migraciones

---
+++ database/migrations/2022_04_04_053752_create_info_values_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_values', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('value_id');
+
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('value_id')->references('id')->on('values')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info_values');
+    }
+}
