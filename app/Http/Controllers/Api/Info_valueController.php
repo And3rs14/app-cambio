dc3bc72338eddf0e117881a41d920c71e09e5c97
Bill Anderson <billbardales@gmail.com>
2022-04-05 11:11:10 -0500

    Se añadió Info_valueController

---
+++ app/Http/Controllers/Api/Info_valueController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Info_value;
+use Illuminate\Http\Request;
+
+class Info_valueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $info_values = Info_value::all();
+        return $info_values;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            //faltaria un unique
+            'date' => 'required|date',
+            //|numeric?
+            'category_id' => 'required|exists:categories,id',
+            'value_id' => 'required|exists:values,id',
+        ]);
+        $info_values = Info_value::create($request->all());
+        return $info_values;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Info_value  $info_value
+     * @return \Illuminate\Http\Response
+     */
+    public function show($info_values)
+    {
+        $info_values = Info_value::select('*')->where('id', $info_values)->get();
+        return $info_values;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Info_value  $info_value
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $info_values)
+    {
+        $request->validate([
+            //faltaria un unique
+            'date' => 'required|date',
+            //|numeric?
+            'category_id' => 'required|exists:categories,id',
+            'value_id' => 'required|exists:values,id',
+        ]);
+
+        $info_values = Info_value::select('*')->where('id', $info_values)->update($request->all());
+        $info_value = Info_value::select('*')->where('id', $info_values)->get();
+
+        //$valor->update($request->all());
+        return $info_value;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Info_value  $info_value
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($info_values)
+    {
+        $info_value = Info_value::select('*')->where('id', $info_values)->get();
+        Info_value::select('*')->where('id', $info_values)->delete();
+        
+        return $info_value;
+    }
+}
